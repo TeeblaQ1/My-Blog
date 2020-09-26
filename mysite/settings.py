5df3190479398b8f434b742be48717510259ebf7
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2020-09-26 16:20:38 -0400

    First version of application moved into github

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'brfxce@=)p!vt#c+fm!&4)-)=4o&d88(qb@i+8#n7fkc4&k3-u'
+# SECRET_KEY = 'brfxce@=)p!vt#c+fm!&4)-)=4o&d88(qb@i+8#n7fkc4&k3-u'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'brfxce@=)p!vt#c+fm!&4)-)=4o&d88(qb@i+8#n7fkc4&k3-u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 SITE_ID = 1
 
